1be20253840e5b379bbbbbc6de0702ff43915167
bum12ark <bum12ark@gmail.com>
2022-10-10 01:03:53 +0900

    Add the number of RetryableException cases

--- src/test/java/com/guide/infra/client/UserClientErrorDecoderTest.java
+++ src/test/java/com/guide/infra/client/UserClientErrorDecoderTest.java
@@ -13,46 +13,65 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.http.HttpStatus;
 
+@TestInstance(Lifecycle.PER_CLASS)
 class UserClientErrorDecoderTest {
 
-    private final Map<String, Collection<String>> headers = new LinkedHashMap<>();
+    private static final Map<String, Collection<String>> HEADERS = new LinkedHashMap<>();
+    private static final String METHOD_KEY = "Service#foo()";
     private final ErrorDecoder errorDecoder = new UserClientErrorDecoder();
 
     @Test
     void throwsFeignException() {
         // GIVEN
-        Response response = createFeignResponse(502, "Bad Gateway Exception");
+        Response response =
+                createFeignResponse(HttpStatus.BAD_GATEWAY.value(), HttpStatus.BAD_GATEWAY.name());
 
         // WHEN & THEN
         assertThrows(
                 FeignException.class,
                 () -> {
-                    throw errorDecoder.decode("", response);
+                    throw errorDecoder.decode(METHOD_KEY, response);
                 });
     }
 
-    @Test
-    void throwsRetryException() {
+    @ParameterizedTest
+    @MethodSource(value = "createRetryStatusMethodSource")
+    void throwsRetryableException(final Response retryResponse) {
         // GIVEN
-        Response response = createFeignResponse(500, "Internal Server Error");
-
         // WHEN & THEN
         assertThrows(
                 RetryableException.class,
                 () -> {
-                    throw errorDecoder.decode("", response);
+                    throw errorDecoder.decode(METHOD_KEY, retryResponse);
                 });
     }
 
-    private Response createFeignResponse(int status, String reason) {
+    Stream<Arguments> createRetryStatusMethodSource() {
+        return Stream.of(
+                Arguments.arguments(
+                        createFeignResponse(
+                                HttpStatus.INTERNAL_SERVER_ERROR.value(), HttpStatus.INTERNAL_SERVER_ERROR.name())),
+                Arguments.arguments(
+                        createFeignResponse(
+                                HttpStatus.TOO_MANY_REQUESTS.value(), HttpStatus.TOO_MANY_REQUESTS.name())));
+    }
+
+    Response createFeignResponse(int status, String reason) {
         return Response.builder()
                 .status(status)
                 .reason(reason)
                 .request(
                         Request.create(HttpMethod.GET, "/api", Collections.emptyMap(), null, Util.UTF_8, null))
-                .headers(headers)
+                .headers(HEADERS)
                 .build();
     }
 }
