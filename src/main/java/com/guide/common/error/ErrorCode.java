3497b5121f98303ca26375374b8683371d2ed8fe
bum12ark <bum12ark@gmail.com>
2022-10-10 18:18:57 +0900

    Add user not found error code

--- src/main/java/com/guide/common/error/ErrorCode.java
+++ src/main/java/com/guide/common/error/ErrorCode.java
@@ -9,12 +9,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @JsonFormat(shape = Shape.OBJECT)
 public enum ErrorCode {
+    // Common
     BAD_REQUEST(4001L, "Invalid Input Value"),
     METHOD_NOT_ALLOWED(4002L, "Request Method Not Supported"),
     NOT_HANDLER_FOUND(4003L, "Non-Existent Api Request"),
     UNKNOWN(9999L, "Unknown Error"),
 
+    // Feign
     RETRY_MAX_ATTEMPTS(5001L, "Max Retry Fail"),
+
+    // User
+    USER_NOT_FOUND(4100L, "User not found during call user-api"),
     USER_CLIENT_UNPROCESSABLE(5002L, "Something wrong during call user-api");
 
     private final long code;
